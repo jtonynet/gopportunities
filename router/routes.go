d91f2260b756557b2232b224cd8873937fc2f90e
jtonyNet <jtony.net@gmail.com>
2023-11-03 01:01:24 -0300

    feat: add handlers

--- router/routes.go
+++ router/routes.go
@@ -1,42 +1,17 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"github.com/jtonynet/gopportunities/handler"
 )
 
 func initializeRoutes(router *gin.Engine) {
 
 	v1 := router.Group("/api/v1")
 
-	v1.GET("/opening", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "GET Openning",
-		})
-	})
-
-	v1.POST("/opening", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "POST Openning",
-		})
-	})
-
-	v1.DELETE("/opening", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "DELETE Openning",
-		})
-	})
-
-	v1.PUT("/opening", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "PUT Openning",
-		})
-	})
-
-	v1.GET("/openings", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, gin.H{
-			"msg": "GET all Opennings",
-		})
-	})
+	v1.GET("/opening", handler.ShowOpeningHandler)
+	v1.POST("/opening", handler.CreateOpeningHandler)
+	v1.DELETE("/opening", handler.DeleteOpeningHandler)
+	v1.PUT("/opening", handler.UpdateOpeningHandler)
+	v1.GET("/openings", handler.ListOpeningHandler)
 }
