9f4497673cb2fdee05416ffb0d3375b2491727cc
jtonyNet <jtony.net@gmail.com>
2023-11-03 02:07:10 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"github.com/jtonynet/gopportunities/config"
 	"github.com/jtonynet/gopportunities/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+	}
+
 	router.Initialize()
 }
